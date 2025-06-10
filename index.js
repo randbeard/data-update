c7b3d74b18dcd315a3ebf5f7b6aa7fb69a24de1b
randbeard <104162980+randbeard@users.noreply.github.com>
2025-06-10 18:51:31 -0400

    yu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.static('build'));
@@ -7,5 +7,3 @@ const path = require('path');
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
-  
-const port = process.env.PORT || 5000;app.listen(port, () => { console.log(`Server started on port: ${port}`);});
