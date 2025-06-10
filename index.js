4d7fdd6ac8adf4adfe7e8439d7488e83201e1f1d
randbeard <104162980+randbeard@users.noreply.github.com>
2025-06-10 18:39:50 -0400

    Update and rename post to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+onst express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+const path = require('path');
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
+  
+const port = process.env.PORT || 5000;app.listen(port, () => { console.log(`Server started on port: ${port}`);});
